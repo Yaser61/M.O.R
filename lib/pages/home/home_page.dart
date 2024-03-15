e8ed085ae92840158a223d518451dc6ef91d3f1f
Yaser <kalkanyaser@gmail.com>
2024-03-15 13:55:39 +0300

    test yaser

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -25,7 +25,7 @@ class _HomePageState extends BaseStatefulState<HomePage> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("First commit"),
+          Text("Test yaser"),
         ],
       ),
     );
